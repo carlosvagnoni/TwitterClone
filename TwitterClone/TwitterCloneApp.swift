bcfe8380e21763598e882d1ebfa7cd23942d2bb7
Carlos Vagnoni <carlosvagnoni11@gmail.com>
2023-05-31 18:21:56 -0400

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by user239477 on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
